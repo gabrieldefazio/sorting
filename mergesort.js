a9ea9321256888815278fe72973023794ac5ecb7
Gabriel DeFazio <aloveleter@gmail.com>
2017-09-17 23:23:43 -0400

    message

--- mergesort.js
+++ mergesort.js
@@ -1,27 +1,31 @@
 const mergeSort = array =>{
-    if (array.length < 2) return array;
-    const splits = split(array),
-        left = splits[0],
-        right = splits[1];
-    return merge(mergeSort(left), mergeSort(right))
-};
+  if (array.length < 2) return array
+  else {
+    array = split(array);
+    let left = array[0];
+    let right = array[1]
+    console.log(array)
+    return merge(mergeSort(left), mergeSort(right));
+  }
+}
 
 const split = array =>{
-    let center = array.lenth / 2;
-    let left = array.slice(0, center);
-    let right = array.slice(center);
-    return [left, right];
-};
+  let center = array.length / 2
+  return [array.slice(0, center), array.slice(center)];
+}
 
 const merge = (left, right) =>{
-    let merged = [];
-    let leftI = 0;
-    let rightI = 0;
-    while(leftI < left.length && rightI < right.length) {
-        if (left[leftI] < right[rightI]) merged.push(left[leftI++])
-        else merged.push(right[rightI++])
-    }
-    for(; leftI < left.length; leftI++) merged.push(left[leftI++]);
-    for(; rightI < right.length; leftI++) merged.push(right[rightI++]);
-    return merged
-};
+  let merged = [];
+  let leftI = 0;
+  let rightI = 0;
+  while (leftI < left.length && rightI < right.length){
+    if (left[leftI] > right[rightI]) merged.push(right[rightI++])
+    else merged.push(left[leftI++])
+  }
+  for(; leftI < left.length; leftI++) merged.push(left[leftI++]);
+  for(; rightI < right.length; leftI++) merged.push(right[rightI++]);
+  return merged
+}
+
+let sorted = mergeSort([39,-1,9,2,64,100,63, -13])
+console.log(sorted)
